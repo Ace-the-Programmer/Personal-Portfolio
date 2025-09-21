d5a5b5e32668d5947740bb8acb0bbe2ae2a94c4e
AcE <acecumbe4@gmail.com>
2025-09-21 19:17:03 +0800

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  project1Image: string | ArrayBuffer | null = null;
+  project2Image: string | ArrayBuffer | null = null;
+  project3Image: string | ArrayBuffer | null = null;
+
   constructor() {}
+
+  onImageSelect(event: any, project: string) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (project === 'project1') this.project1Image = reader.result;
+        if (project === 'project2') this.project2Image = reader.result;
+        if (project === 'project3') this.project3Image = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
